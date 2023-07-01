51db1667c90cc13e72e0ed364fa53978bc9cbdb9
josuerushanika <josuerushanika@gmail.com>
2023-07-01 22:32:02 +0200

    Update post images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ApplicationRecord
 
 
-    validates :title, presence: true
-    validates :description, presence: true
-    validates :keywords, presence: true
+    validates :title, presence: true, length: { minimum: 3, maximum: 100}
+    validates :description, presence: true, length: { minimum: 3, maximum: 100}
+    validates :keywords, presence: true, length: { minimum: 3, maximum: 100}
+
+    has_many_attached :images
 end
